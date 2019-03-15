ed47563aea3369759c3e80f64fbb2e73668a0c6c
Parimal Desai <parimalvfx@gmail.com>
2019-03-15 23:08:48 +0530

    Added getCategoryById endpoint in Category Controller

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
@@ -1,17 +1,15 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
-import com.upgrad.FoodOrderingApp.api.model.CategoriesListResponse;
-import com.upgrad.FoodOrderingApp.api.model.CategoryListResponse;
+import com.upgrad.FoodOrderingApp.api.model.*;
 import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,7 +22,7 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * This api endpoint is used retrieve all the categories present in the database, ordered by their name
+     * This api endpoint is used to retrieve all the categories present in the database, ordered by their name
      *
      * @return ResponseEntity<CategoriesListResponse> type object along with HttpStatus OK
      */
@@ -46,5 +44,36 @@ public class CategoryController {
         }
 
         return new ResponseEntity<CategoriesListResponse>(categoriesListResponse, HttpStatus.OK);
+    }
+
+    /**
+     * This api endpoint is used to retrieve category for given id with all items within that category
+     *
+     * @param categoryId ID of category
+     *
+     * @return ResponseEntity<CategoryDetailsResponse> type object along with HttpStatus OK
+     *
+     * @throws CategoryNotFoundException If category is not found for given ID
+     */
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, path = "/category/{category_id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<CategoryDetailsResponse> getCategoryById(@PathVariable("category_id") final String categoryId) throws CategoryNotFoundException {
+
+        // get all items for category
+        CategoryEntity categoryEntity = categoryService.getCategoryById(categoryId);
+
+        // create response
+        CategoryDetailsResponse categoryDetailsResponse = new CategoryDetailsResponse().id(UUID.fromString(categoryEntity.getUuid())).categoryName(categoryEntity.getCategoryName());
+
+        for (ItemEntity itemEntity : categoryEntity.getItems()) {
+            ItemList itemList = new ItemList()
+                    .id(UUID.fromString(itemEntity.getUuid()))
+                    .itemName(itemEntity.getItemName())
+                    .price(itemEntity.getPrice())
+                    .itemType(ItemList.ItemTypeEnum.fromValue(itemEntity.getType().getValue()));
+            categoryDetailsResponse.addItemListItem(itemList);
+        }
+
+        return new ResponseEntity<CategoryDetailsResponse>(categoryDetailsResponse, HttpStatus.OK);
     }
 }
